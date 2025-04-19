0d3eb6f3972b3d09d996d693233e1789a56af82e
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2025-04-19 16:08:08 +0300

    Updates

---
+++ parcel-progect.816e7b21.js
@@ -0,0 +1,3 @@
+console.log('Hello world!');
+
+//# sourceMappingURL=parcel-progect.816e7b21.js.map
---
+++ parcel-progect.e3972529.js
@@ -0,0 +1,2 @@
+console.log("Hello world!");
+//# sourceMappingURL=parcel-progect.e3972529.js.map
